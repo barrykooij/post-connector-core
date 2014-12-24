f822857195efb72964ffd56d31055712b6aabba5
Remy van Velthuijsen <remy@remysplace.de>
2014-12-24 14:59:46 +0100

    Allows return the variable filtered

--- classes/filters/class-filter-shortcode-tiny-button.php
+++ classes/filters/class-filter-shortcode-tiny-button.php
@@ -9,10 +9,10 @@ class SP_Filter_Shortcode_Tiny_Button extends SP_Filter {
 
 	public function run( $buttons ) {
 		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
-			return;
+			return $buttons;
 		}
 		if ( get_user_option( 'rich_editing' ) != 'true' ) {
-			return;
+			return $buttons;
 		}
 
 		array_push( $buttons, "|", "post_connector_shortcodes_button" );
