9a93f86997d93e6e0af1c651fc09ceef70c111da
Barry Kooij <git@barrykooij.nl>
2015-02-28 12:51:47 +0100

    Grunt and package file

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+/* jshint node:true */
+module.exports = function (grunt) {
+	'use strict';
+
+	grunt.initConfig({
+		// setting folder templates
+		dirs: {
+			css: 'assets/css',
+			images: 'assets/images',
+			js: 'assets/js'
+		},
+
+		// Minify all .css files.
+		cssmin: {
+			minify: {
+				expand: true,
+				cwd: '<%= dirs.css %>/',
+				src: ['*.css'],
+				dest: '<%= dirs.css %>/',
+				ext: '.css'
+			}
+		},
+
+		// Minify .js files.
+		uglify: {
+			options: {
+				preserveComments: 'some'
+			},
+			jsfiles: {
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.js %>/',
+					src: [
+						'*.js',
+						'!*.min.js',
+						'!Gruntfile.js',
+					],
+					dest: '<%= dirs.js %>/',
+					ext: '.min.js'
+				}]
+			}
+		},
+
+	});
+
+	// Load NPM tasks to be used here
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Register tasks
+	grunt.registerTask('default', [
+		'cssmin',
+		'uglify'
+	]);
+
+};
