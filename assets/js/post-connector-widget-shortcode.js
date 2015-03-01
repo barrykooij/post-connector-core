c6f8213c53a576af9979012da79db2b81f1004d3
Barry Kooij <git@barrykooij.nl>
2015-03-01 21:18:30 +0100

    JS changes to support multiple type of widgets

--- assets/js/post-connector-widget-shortcode.js
+++ assets/js/post-connector-widget-shortcode.js
@@ -1,75 +1,81 @@
-jQuery(function($) {
-	SubPostsBindParentAjaxForms();
-});
+jQuery( function ( $ ) {
+    PC_Bind_Update_Select();
+} );
 
-function SubPostsBindParentAjaxForms() {
-	jQuery.each(jQuery('.sp_showchilds_ajax'), function(k,v) {
-		new SubPostsAjaxForm(v);
-	});
+function PC_Bind_Update_Select() {
+    jQuery.each( jQuery( '.pc_ajax_child' ), function ( k, v ) {
+        new PC_Update_Select( v, 'child' );
+    } );
+    jQuery.each( jQuery( '.pc_ajax_parent' ), function ( k, v ) {
+        new PC_Update_Select( v, 'parent' );
+    } );
 }
 
-jQuery(document).ajaxSuccess(function(e, xhr, settings) {
-	var widget_id_base = 'sp_widget_showchilds';
+jQuery( document ).ajaxSuccess( function ( e, xhr, settings ) {
+    var widget_id_base = 'sp_widget_showchilds';
 
-	if( undefined != settings.data && settings.data.search('action=save-widget') != -1 && settings.data.search('id_base=' + widget_id_base) != -1) {
-		SubPostsBindParentAjaxForms();
-	}
-});
+    if ( undefined != settings.data && settings.data.search( 'action=save-widget' ) != -1 && settings.data.search( 'id_base=' + widget_id_base ) != -1 ) {
+        PC_Bind_Update_Select();
+    }
+} );
 
 /**
- * Used on post screen (shortcode) and widget screen (widget)
+ * Used on post screen (shortcode) and widget screen (widget) for updating children
  *
  * @param tgt
  * @constructor
  */
-function SubPostsAjaxForm( tgt ) {
+function PC_Update_Select( tgt, type ) {
 
-	this.widget = tgt;
+    this.widget = tgt;
+    this.type = type;
 
-	this.init = function() {
-		this.bind();
-	};
+    this.init = function () {
+        this.bind();
+    };
 
-	this.bind = function() {
-		var instance = this;
-		jQuery(this.widget).find('.postlink').bind('change', function() {
-			instance.update_parents(this);
-		});
-	};
+    this.bind = function () {
+        var instance = this;
+        jQuery( this.widget ).find( '.postlink' ).bind( 'change', function () {
+            instance.update_select( this );
+        } );
+    };
 
-	this.update_parents = function(tgt) {
+    this.update_select = function ( tgt ) {
 
-		var instance = this;
+        var instance = this;
 
-		var opts = {
-			url: ajaxurl,
-			type: 'POST',
-			async: true,
-			cache: false,
-			dataType: 'json',
-			data:{
-				action: 'get_parent_posts',
-				identifier: jQuery(tgt).val(),
-				nonce: jQuery(instance.widget).find('#sp_widget_child_nonce').val(),
-				by_slug : ( jQuery(instance.widget).find('#by_slug').val() ) ? jQuery(instance.widget).find('#by_slug').val() : false
-			},
-			success: function(response) {
-				var select_parent = jQuery(instance.widget).find('.parent');
+        var pc_action = ( 'parent' == this.type ) ? 'pc_get_child_posts' : 'pc_get_parent_posts';
 
-				jQuery(select_parent).empty();
-				jQuery(select_parent).append(jQuery('<option>').val('current').html(sp_js.current_page));
-				jQuery.each( response, function( index, value ) {
-					jQuery(select_parent).append(
-							jQuery('<option>').val(index).html(value)
-					)
-				} );
-			},
-			error: function(xhr,textStatus,e) {
-				return;
-			}
-		};
-		jQuery.ajax(opts);
-	};
+        var opts = {
+            url: ajaxurl,
+            type: 'POST',
+            async: true,
+            cache: false,
+            dataType: 'json',
+            data: {
+                action: pc_action,
+                identifier: jQuery( tgt ).val(),
+                nonce: jQuery( instance.widget ).find( '#sp_widget_child_nonce' ).val(),
+                by_slug: ( jQuery( instance.widget ).find( '#by_slug' ).val() ) ? jQuery( instance.widget ).find( '#by_slug' ).val() : false
+            },
+            success: function ( response ) {
+                var select_parent = jQuery( instance.widget ).find( '.' + instance.type );
 
-	this.init();
+                jQuery( select_parent ).empty();
+                jQuery( select_parent ).append( jQuery( '<option>' ).val( 'current' ).html( sp_js.current_page ) );
+                jQuery.each( response, function ( index, value ) {
+                    jQuery( select_parent ).append(
+                        jQuery( '<option>' ).val( index ).html( value )
+                    )
+                } );
+            },
+            error: function ( xhr, textStatus, e ) {
+                return;
+            }
+        };
+        jQuery.ajax( opts );
+    };
+
+    this.init();
 }
