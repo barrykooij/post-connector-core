4bb36ce9356022ad773eab4edba29151e85ddb31
Barry Kooij <git@barrykooij.nl>
2015-04-11 12:07:01 +0200

    Make shortcode use generate_list method

--- classes/shortcodes/class-shortcode-show-children.php
+++ classes/shortcodes/class-shortcode-show-children.php
@@ -11,6 +11,7 @@ class SP_Shortcode_Show_Children extends SP_Shortcode {
 			'parent'  => null,
 			'link'    => 'true',
 			'excerpt' => 'true',
+			'image' => 'false'
 	);
 
 	public function run( $atts ) {
@@ -21,12 +22,12 @@ class SP_Shortcode_Show_Children extends SP_Shortcode {
 			return;
 		}
 
-		if ( $parent === null ) {
+		if ( null === $parent ) {
 			$parent = get_the_ID();
 		}
 
 		$post_link_manager = new SP_Post_Link_Manager();
 
-		return $post_link_manager->generate_children_list( $slug, $parent, $link, $excerpt );
+		return $post_link_manager->generate_children_list( $slug, $parent, $link, $excerpt, 'b', $image );
 	}
 }
